9b368f9da3aa64c20e8470c4ce3baebeb805b8ef
Katya Sheps <katya@sheps.ru>
2019-06-02 17:04:10 +0300

    Initial commit

---
+++ app/src/main/java/ru/sheps/android/homework341/MainActivity.java
@@ -0,0 +1,37 @@
+package ru.sheps.android.homework341;
+
+import android.content.res.Configuration;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void btnOk(View view) {
+        Locale locale;
+        Spinner spinner = findViewById(R.id.spinner);
+        int position = spinner.getSelectedItemPosition();
+        if (position == 1) {
+            locale = new Locale("ru");
+        } else {
+            locale = new Locale("eng");
+        }
+        Configuration config = new Configuration();
+        config.setLocale(locale);
+        getResources().updateConfiguration(config, getBaseContext().getResources().getDisplayMetrics());
+        recreate();
+
+    }
+
+}
